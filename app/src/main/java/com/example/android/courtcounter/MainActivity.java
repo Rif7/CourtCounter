1c4e59f94c3e41f5d60357b289e22040f28032d2
Rif7 <rifraf92@gmail.com>
2017-11-29 22:09:01 +0100

    Created layout and simple logic

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        score = 0;
+        displayPoints(score);
     }
+
+    private void displayPoints(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.score);
+        quantityTextView.setText("" + number);
+    }
+
+    private void increaseDisplayedScore(int addedPoints) {
+        score += addedPoints;
+        displayPoints(score);
+    }
+
+    public void plusThreePoints(View view) {
+        increaseDisplayedScore(3);
+    }
+
+    public void plusTwoPoints(View view) {
+        increaseDisplayedScore(2);
+    }
+
+    public void plusOnePoint(View view){
+        increaseDisplayedScore(1);
+    }
+
 }
