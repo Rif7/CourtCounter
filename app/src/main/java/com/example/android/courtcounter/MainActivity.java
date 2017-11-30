5faf49b4e9fbf94de4fe65cdaa508ccb5055f65e
Rif7 <rifraf92@gmail.com>
2017-11-30 23:50:16 +0100

    Added second team to layout and refactored code

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -5,37 +5,73 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
-    int score;
+    class TeamScoreCounter {
+        private TextView scoreTextView;
+        private int score;
+
+        TeamScoreCounter(String scoreTextViewId) {
+            int resId = getResources().getIdentifier(scoreTextViewId, "id", getPackageName());
+            this.scoreTextView = (TextView) findViewById(resId);
+            resetScore();
+        }
+
+        private void resetScore() {
+            score = 0;
+        }
+
+        private void increaseScore(int addedScore) {
+            score += addedScore;
+        }
+
+        private void displayPoints() {
+            scoreTextView.setText("" + score);
+        }
+
+    }
+
+    private TeamScoreCounter scoreTeamA;
+    private TeamScoreCounter scoreTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        score = 0;
-        displayPoints(score);
+        scoreTeamA = new TeamScoreCounter("scoreTeamA");
+        scoreTeamB = new TeamScoreCounter("scoreTeamB");
+        scoreTeamA.displayPoints();
+        scoreTeamB.displayPoints();
     }
 
-    private void displayPoints(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.score);
-        quantityTextView.setText("" + number);
+    public void plusThreePointsTeamA(View view) {
+        scoreTeamA.increaseScore(3);
+        scoreTeamA.displayPoints();
     }
 
-    private void increaseDisplayedScore(int addedPoints) {
-        score += addedPoints;
-        displayPoints(score);
+    public void plusTwoPointsTeamA(View view) {
+        scoreTeamA.increaseScore(2);
+        scoreTeamA.displayPoints();
     }
 
-    public void plusThreePoints(View view) {
-        increaseDisplayedScore(3);
+    public void plusOnePointTeamA(View view) {
+        scoreTeamA.increaseScore(1);
+        scoreTeamA.displayPoints();
     }
 
-    public void plusTwoPoints(View view) {
-        increaseDisplayedScore(2);
+    public void plusThreePointsTeamB(View view) {
+        scoreTeamB.increaseScore(3);
+        scoreTeamB.displayPoints();
     }
 
-    public void plusOnePoint(View view){
-        increaseDisplayedScore(1);
+    public void plusTwoPointsTeamB(View view) {
+        scoreTeamB.increaseScore(2);
+        scoreTeamB.displayPoints();
+    }
+
+    public void plusOnePointTeamB(View view) {
+        scoreTeamB.increaseScore(1);
+        scoreTeamB.displayPoints();
     }
 
 }
